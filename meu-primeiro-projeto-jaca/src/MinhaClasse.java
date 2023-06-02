9364189ea667bedce142c2e20b1b1c48ac4b79ee
Brusios <32931426+Brusios@users.noreply.github.com>
2023-06-02 20:03:23 -0300

    Primeira Classe

--- meu-primeiro-projeto-jaca/src/MinhaClasse.java
+++ meu-primeiro-projeto-jaca/src/MinhaClasse.java
@@ -1,5 +1,14 @@
 public class MinhaClasse {
   public static void main (String [] args){
-    System.out.print("Ola turma, sejam bem-vindos");
+    //System.out.print("Ola turma, sejam bem-vindos");
+  
+    String primeiroNome = "Bruno";
+    String segundoNome = "Gustavo";
+
+    String nomeCompleto = nomeClompeto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+  }
+  public static String nomeClompeto (String primeiroNome, String segundoNome){
+    return "Nome completo: " + primeiroNome.concat(" ").concat(segundoNome);
   }
 }
