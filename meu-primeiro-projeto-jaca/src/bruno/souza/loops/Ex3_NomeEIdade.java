a277db73bd8e88d8b368673755bb8d8ee04dce62
Brusios <32931426+Brusios@users.noreply.github.com>
2023-06-07 00:13:25 -0300

    Do while calculando e comparando dentro do looping

---
+++ meu-primeiro-projeto-jaca/src/bruno/souza/loops/Ex3_NomeEIdade.java
@@ -0,0 +1,30 @@
+package bruno.souza.loops;
+
+import java.util.Scanner;
+
+public class Ex3_NomeEIdade {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int numero;
+    int count = 0;
+    int maior = 0;
+    int soma = 0;
+
+      do {
+        System.out.println("Numero: ");
+        numero = scan.nextInt();
+
+        soma = soma + numero;
+        if (numero > maior) maior = numero;
+        
+        count = count + 1;       
+      } while (count < 5);
+
+      System.out.println("Maior numero: " + maior);
+      System.out.println("Soma total de todos os numeros: " + soma);
+      System.out.println("Media entre os 5 numeros digitados: " + (soma/5));
+    }
+
+  
+}
