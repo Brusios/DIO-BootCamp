b67f66e7e2fc281d563f1816bc8c15f8618246a0
Brusios <32931426+Brusios@users.noreply.github.com>
2023-06-07 01:21:44 -0300

    Matriz multidimensional

---
+++ meu-primeiro-projeto-jaca/src/bruno/souza/array/Ex4.java
@@ -0,0 +1,26 @@
+package bruno.souza.array;
+import java.util.Random;
+/*
+ * Gerando e imprimindo uma matriz M 3x3 com valores aleatorios entre 0-9
+ */
+public class Ex4 {
+  public static void main(String[] args) {
+    Random random = new Random();
+
+    int[][] M = new int[4][4];
+
+    for (int i = 0; i < M.length; i++) {
+      for (int j = 0; j < M[i].length; j++) {
+        M[i][j] = random.nextInt(9); 
+      }
+    }
+
+    System.out.println("Matriz: ");
+    for (int[] linha : M) {
+      for (int coluna : linha) {
+        System.out.print(coluna + " ");
+      }
+      System.out.println();
+    }
+  }
+}
