a70a7c938c87105011e59f98aae70d2bdc41e72f
Brusios <32931426+Brusios@users.noreply.github.com>
2023-06-07 00:38:58 -0300

    Fatorial com looping em for

---
+++ meu-primeiro-projeto-jaca/src/bruno/souza/loops/Ex6_NomeEIdade.java
@@ -0,0 +1,20 @@
+package bruno.souza.loops;
+
+import java.util.Scanner;
+
+public class Ex6_NomeEIdade {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Fatorial: ");
+    int fatorial = scan.nextInt();
+    int multiplicacao = 1;
+
+    for (int i = fatorial; i > 1 ; i--) {
+      multiplicacao = multiplicacao * i;
+      //System.out.println();
+    }
+    System.out.println(fatorial +"! = " + multiplicacao);
+  
+  }  
+}
