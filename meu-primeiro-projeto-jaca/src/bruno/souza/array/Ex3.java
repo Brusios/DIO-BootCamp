620f58f1a635dc911011202284b99c508898700e
Brusios <32931426+Brusios@users.noreply.github.com>
2023-06-07 01:12:49 -0300

    Numeros aleatorios com randon utilizando arrays

---
+++ meu-primeiro-projeto-jaca/src/bruno/souza/array/Ex3.java
@@ -0,0 +1,29 @@
+package bruno.souza.array;
+import java.util.Random;
+
+public class Ex3 {
+  public static void main(String[] args) {
+    Random random = new Random();
+    int[] numerosAleatorios = new int[14];
+
+    for (int i = 0; i < numerosAleatorios.length; i++) {
+      int numero = random.nextInt(100);
+      numerosAleatorios[i] = numero;
+    }
+
+    System.out.println("Numeros Aleatorios: ");
+    for (int numero : numerosAleatorios) {
+      System.out.print(numero + " | ");
+    }       
+
+    System.out.println("\nSucessores dos Numeros Aleatorios: ");
+    for (int numero : numerosAleatorios) {
+      System.out.print((numero+1) + " | ");
+    }      
+    
+    System.out.println("\nAntecessor dos Numeros Aleatorios: ");
+    for (int numero : numerosAleatorios) {
+      System.out.print((numero-1) + " | ");
+    } 
+  }
+}
