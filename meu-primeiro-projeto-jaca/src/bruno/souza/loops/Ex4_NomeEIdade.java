0acada89fee84b5ea512e9fd73033f28f6cb5ac9
Brusios <32931426+Brusios@users.noreply.github.com>
2023-06-07 00:25:30 -0300

    Contabilizando quantidade de numeros impar e par

---
+++ meu-primeiro-projeto-jaca/src/bruno/souza/loops/Ex4_NomeEIdade.java
@@ -0,0 +1,33 @@
+package bruno.souza.loops;
+
+import java.util.Scanner;
+
+public class Ex4_NomeEIdade {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int quantNumeros;
+
+    System.out.println("Quabtidade de numeros: ");
+    quantNumeros = scan.nextInt();   
+    
+    int count = 0;
+    int numero = 0;
+    int quantPares = 0;
+    int quantImpares = 0;
+
+    do {
+      System.out.println("Numero: ");
+      numero = scan.nextInt();
+
+      if(numero % 2 == 0) quantPares++; //Add a variavel quantPares
+      else quantImpares++;
+
+      count ++;
+      //count = count +1;
+
+    } while (count < quantNumeros);
+    System.out.println("Quantidade Par: " + quantPares);
+    System.out.println("Quantidade Impar: " + quantImpares);
+  }  
+}
