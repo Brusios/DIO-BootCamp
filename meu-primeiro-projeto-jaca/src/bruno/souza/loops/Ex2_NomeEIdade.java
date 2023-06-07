89defec852b4e5deb9141a70cbdc1a1dfbb19308
Brusios <32931426+Brusios@users.noreply.github.com>
2023-06-07 00:03:29 -0300

    Looping validando numero entre 0 a 10

---
+++ meu-primeiro-projeto-jaca/src/bruno/souza/loops/Ex2_NomeEIdade.java
@@ -0,0 +1,18 @@
+package bruno.souza.loops;
+
+import java.util.Scanner;
+
+public class Ex2_NomeEIdade {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int nota;
+      System.out.println("Nota: ");
+      nota = scan.nextInt();
+
+      while (nota < 0 || nota > 10) {
+        System.out.println("Nota invalida! Digite um numero entre 0 a 10.");
+        nota = scan.nextInt();
+      }
+  }
+}
